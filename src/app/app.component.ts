f779168b60a6101316c78adf59393629736cb23d
MarinaBuyakevich <manyunya1011@gmail.com>
2019-01-11 16:51:28 +0200

    is form done

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+   templateUrl: './app.component.html',
+   styleUrls: ['./app.component.css'],
+})
+
+export class AppComponent {
+  Appearance = true;
+  Behavior = true;
+  Columns = true;
+  Columns_Color = true;
+  button_1 = false;
+  button_2 = false;
+  button_3 = false;
+  button_4 = false;
+  button_5 = false;
+  button_6 = false;
+}
